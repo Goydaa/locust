afdb1b4c55f8bd601f190fdee435a3099a29d8df
Goydaa <URNAZXC@GMAIL.COM>
2026-03-16 23:03:26 +0300

    init: полный проект нагрузочного тестирования Locust

--- src/locustfile.py
+++ src/locustfile.py
@@ -2,32 +2,32 @@ from locust import HttpUser, task, between
 import random 
  
 class BlogUser(HttpUser): 
-    Виртуальный пользователь блога. 
-    При заходе на сайт получает список всех постов. 
-    Потом в 70% случаев смотрит список, в 30% - читает конкретный пост. 
-    wait_time = between(1, 5)  # пользователь думает 1-5 секунд 
+    # Virtual blog user 
+    # On start, gets list of all posts 
+    # Then in 70%% cases views list, in 30%% - views single post 
+    wait_time = between(1, 5)  # user thinks 1-5 seconds 
  
     def on_start(self): 
-        """Выполняется при старте каждого пользователя""" 
+        # Executed when each user starts 
         response = self.client.get("/posts") 
         if response.status_code == 200: 
-            self.posts_list = response.json()  # сохраняем список постов 
-            print(f"Пользователь {self} загрузил {len(self.posts_list)} постов") 
+            self.posts_list = response.json()  # save posts list 
+            print(f"User {self} loaded {len(self.posts_list)} posts") 
         else: 
             self.posts_list = [] 
-            print(f"Ошибка загрузки списка постов: {response.status_code}") 
+            print(f"Error loading posts: {response.status_code}") 
  
     @task(7) 
     def get_all_posts(self): 
-        """Посмотреть список всех постов (частое действие)""" 
+        # View list of all posts (frequent action) 
         self.client.get("/posts") 
  
     @task(3) 
     def get_single_post(self): 
-        """Посмотреть конкретный пост (редкое действие)""" 
-        if self.posts_list:  # если список загружен 
-            # выбираем случайный пост из реально существующих 
+        # View single post (rare action) 
+        if self.posts_list:  # if list is loaded 
+            # choose random post from real existing ones 
             post_id = random.choice(self.posts_list)['id'] 
-        else:  # если что-то пошло не так 
-            post_id = random.randint(1, 100)  # берем случайное число 
+        else:  # if something went wrong 
+            post_id = random.randint(1, 100)  # take random number 
         self.client.get(f"/posts/{post_id}") 
